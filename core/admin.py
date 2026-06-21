7fd5547d7a02a7c03d2dc8d2da56a6608a0f2f9d
Fayis <your_email@example.com>
2026-06-21 14:31:33 +0530

    Add models migrations and admin setup

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
-
 # Register your models here.
+
+from django.contrib import admin
+from .models import User, Job, Application
+
+admin.site.register(User)
+admin.site.register(Job)
+admin.site.register(Application)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,29 @@
+# Create your models here.
+
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Job(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Application(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    applied_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.name} - {self.job.title}"
