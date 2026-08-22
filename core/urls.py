e86bd623f5db71c3f2759469c4067693895069f5
Fayis <your_email@example.com>
2026-08-22 14:44:28 +0530

    Fix doubled api/ prefix in Swagger URLs

--- core/urls.py
+++ core/urls.py
@@ -128,8 +128,7 @@ urlpatterns = [
     path("admin/billing/transactions/<int:pk>/refund/",AdminRefundAPIView.as_view(),),
     path('token/refresh/', TokenRefreshView.as_view()),
     path('logout/', LogoutAPIView.as_view()),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-   
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
