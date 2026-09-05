66a2927af6a95d81c215faba7dfcc937c8e562d5
Fayis <your_email@example.com>
2026-09-05 14:01:24 +0530

    Fix: remove simulated crash in FeaturedJobAPIView

--- core/views/jobs.py
+++ core/views/jobs.py
@@ -116,7 +116,6 @@ class FeaturedJobAPIView(APIView):
 
     def get(self, request):
 
-        x = 1 / 0  # TEMPORARY - simulating a crash for incident training
 
         jobs = Job.objects.filter(status=Job.ACTIVE, is_featured=True).order_by(
             "-created_at"
