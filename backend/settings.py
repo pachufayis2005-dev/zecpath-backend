f12ab3b16655d6280a3958ad81e704d840cc5f83
Fayis <your_email@example.com>
2026-09-08 09:59:08 +0530

    Add DuckDNS domain to ALLOWED_HOSTS

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = ["13.201.147.10", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["13.201.147.10", "zecpath-fayis.duckdns.org", "127.0.0.1", "localhost"]
 
 # Application definition
 
